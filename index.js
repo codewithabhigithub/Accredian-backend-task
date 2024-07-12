aa54e7b8852f6bc33760662f7925d92f631fca58
Abhishek Gupta <109487291+codewithabhigithub@users.noreply.github.com>
2024-07-12 18:59:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PrismaClient } = require('@prisma/client');
+const nodemailer = require('nodemailer');
+require('dotenv').config(); // Use dotenv to manage environment variables
+
+const app = express();
+const prisma = new PrismaClient();
+app.use(bodyParser.json());
+
+app.post('/referrals', async (req, res) => {
+  console.log("AAA", req.body);
+  const { name, email, referralMessage } = req.body;
+
+  if (!name || !email || !referralMessage) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  try {
+    const referral = await prisma.referral.create({
+      data: {
+        name,
+        email,
+        referralMessage,
+      },
+    });
+
+    // Send email notification
+    await sendReferralEmail(name, email, referralMessage);
+
+    res.status(201).json(referral);
+  } catch (error) {
+    console.error('Error creating referral:', error); // Improved error logging
+    res.status(500).json({ error: 'Error creating referral' });
+  }
+});
+
+const sendReferralEmail = async (name, email, message) => {
+  console.log(email);
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: 'New Referral',
+    text: `You have received a new referral from ${name} (${email}). Message: ${message}`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
